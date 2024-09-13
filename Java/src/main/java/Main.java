263b40e27ed0ad3e7b8e5e1300f7dec5e8076e58
Kimi Osti <kimi.osti@studio.unibo.it>
2024-09-13 19:38:54 +0200

    Changed Main class to add initial login page

--- Java/src/main/java/Main.java
+++ Java/src/main/java/Main.java
@@ -1,9 +1,26 @@
+import java.sql.Connection;
+
 import data.DAOUtils;
+import data.LoginInfo;
 import model.Model;
 
 public class Main {
     public static void main(String[] args) {
-        var connection = DAOUtils.localMySQLConnection("federgolf", "root", "Kimi_2003");
+        View.databaseLoginPage();
+    }
+
+    public static void handleLoginData(String username, String password) {
+        Connection connection = null;
+        try {
+            connection = DAOUtils.localMySQLConnection("federgolf", username, password);
+        } catch (Exception e) {
+            View.loginDataError();
+        }
+
+        runApp(connection);
+    }
+
+    public static void runApp(Connection connection) {
         var model = Model.fromConnection(connection);
         var view = new View(() -> {
             try {
